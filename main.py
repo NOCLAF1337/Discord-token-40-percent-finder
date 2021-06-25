167f0a3df48e525cd12006ca23dcc351ff2e1dd4
NOCLAF1337 <86473077+NOCLAF1337@users.noreply.github.com>
2021-06-25 14:41:41 +0200

    main.py update
    added an ASCII art, changed "first part" in "first 40%" at line 18

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import base64
 
-ID = input("ID : ")
+print(''' 
+ _  _  _____  ___  __      __    ____  __  ___  ___  ___ 
+( \( )(  _  )/ __)(  )    /__\  ( ___)/  )(__ )(__ )(__ )
+ )  (  )(_)(( (__  )(__  /(__)\  )__)  )(  (_ \ (_ \ / / 
+(_)\_)(_____)\___)(____)(__)(__)(__)  (__)(___/(___/(_/   
+ 
+ 
+ ''')
+
+ID = input("Enter the ID : ")
 
 Token = base64.b64encode(bytes(ID, 'utf-8'))
 
 Token = Token.decode("utf-8")
 
-print('the first part of the ID' ,ID, 'token is', Token)
+print('the first 40% of the ID' ,ID, 'token is', Token)
